27611495d6f45918aced074ad2c8f6a81e4ef887
praveen <praveen22468@gmail.com>
2017-09-17 16:24:21 +0530

    Added Surefire plugin to pom.xml

--- src/test/java/crm/testcases/LoginTest.java
+++ src/test/java/crm/testcases/LoginTest.java
@@ -1,29 +1,46 @@
 package crm.testcases;
 
 import org.testng.annotations.Test;
+
 import org.testng.Assert;
+
 import org.testng.annotations.DataProvider;
 
+
 public class LoginTest {
-  @Test(dataProvider = "dp")
-  public void f(Integer n, String s) {
-	  
+ 
+ @Test(dataProvider = "dp")
+  
+public void f(Integer n, String s) {
+	
+  
 	  
 
   }
 
-  @Test
-  public void f2(Integer n, String s) {
-	  
+  
+@Test
+  
+public void f2(Integer n, String s) {
+	
+ 
 	  Assert.assertEquals("actual", "expected");
 
-  }
+ 
+ }
   
-  @DataProvider
-  public Object[][] dp() {
-    return new Object[][] {
-      new Object[] { 1, "a" },
-      new Object[] { 2, "b" },
-    };
-  }
+ 
+ @DataProvider
+  public Object[][] dp() 
+{
+   
+ return new Object[][] {
+    
+  new Object[] { 1, "a" },
+   
+  new Object[] { 2, "b" },
+    
+};
+  
+}
 }
